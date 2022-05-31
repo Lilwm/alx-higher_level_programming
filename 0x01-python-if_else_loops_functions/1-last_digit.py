59b5dcf4235feb89c54f8fcbf2bbc13f15ee48a4
Lilwm <lmiiri95@gmail.com>
2022-05-31 17:32:03 +0000

    initial commit

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+num = number%10 if number >= 0 else (number * -1) % 10
+
+if num > 5:
+    print("Last digit of" ,number, "is" ,num, "and is greater than 5")
+elif num == 0:
+    print("Last digit of" ,number, "is" ,num, "and is zero")
+elif num < 6 and num != 0:
+    print("Last digit of" ,number, "is" ,num, "and is less than 6 and not 0")
+
+
---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+for c in range(97,123):
+    if c not in (101,113): 
+        print(chr(c),end='')
