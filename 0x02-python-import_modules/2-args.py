4516ae77cb3349eb47c8145891b5bf74b8bf987c
Lilwm <lmiiri95@gmail.com>
2022-06-02 16:35:29 +0000

    fixed pycode

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,15 +4,13 @@ if __name__ == "__main__":
     import sys
 
     arg_count = len(sys.argv[1:])
-    index = 1
 
     if arg_count == 0:
         print(" 0 arguments.")
-    else:
-        if arg_count == 1:
+    elif arg_count == 1:
             print(" 1 argument:")
-        else:
-            print("{} arguments:".format(arg_count), end="\n")
-        for argument in sys.argv[1:]:
-            print("{}: {}".format(index, argument), end="\n")
-            index = index + 1
+    else:
+        print("{} arguments:".format(arg_count))
+    for index in range(arg_count):
+            print("{}: {}".format(index + 1, sys.argv[index + 1]))
+        
