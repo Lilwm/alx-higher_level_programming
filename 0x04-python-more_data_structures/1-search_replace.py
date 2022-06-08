617c08085f1be42e40c28fef00cdcfef15e55c1b
Lilwm <lmiiri95@gmail.com>
2022-06-08 18:07:14 +0000

    task 5 + pycode fixes

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = my_list[:]
-    
     for item in range(len(new_list)):
         if new_list[item] == search:
             new_list[item] = replace
--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]): 
     result = 0
-
     for item in set(my_list):
         result += item
-    return result
+    return (result)
