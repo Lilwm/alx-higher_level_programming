b1f09207fe51796aa2ab3934801d09ffb4949535
Lilwm <lmiiri95@gmail.com>
2022-06-26 06:17:07 +0000

    corrected output format

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -10,5 +10,5 @@ def safe_function(fct, *args):
         return result
     except Exception as error:
         import sys
-        print("Exception {}".format(error), file=sys.stderr)
+        print("Exception: {}".format(error), file=sys.stderr)
         return None
