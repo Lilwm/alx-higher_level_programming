219123402aeef072e239c26546b8081f8ee6ae41
Lilwm <lmiiri95@gmail.com>
2022-07-04 18:47:50 +0000

    pycode documentation fix

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -3,8 +3,7 @@
 
 
 def inherits_from(obj, a_class):
-    """ returns True if the object is a subclass otherwise False"""
-    
+    """ returns True if the object is a subclass otherwise False"""   
     if type(obj) is a_class or not isinstance(obj, a_class):
         return False
     else:
