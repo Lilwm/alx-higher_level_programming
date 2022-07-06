6f5b712e296f8034f1d48b153e741d49334972ff
Lilwm <lmiiri95@gmail.com>
2022-07-06 08:13:54 +0000

    documentation fix

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -7,4 +7,3 @@ def save_to_json_file(my_obj, filename):
     """ writes an object to a text file using JSON"""
     with open(filename, 'w', encoding="utf-8") as savefile:
         return json.dump(my_obj, savefile)
-
