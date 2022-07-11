6e6f9445055818b12b3b30b82020f9279df5dc3e
Lilwm <lmiiri95@gmail.com>
2022-07-11 06:37:47 +0000

    pycode validation

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -3,18 +3,17 @@
 
 
 def pascal_triangle(n):
-    """returns a list of lists of integers 
-    representing the Pascal’s triangle of n"""
+    """returns a list of lists of integers in Pascal’s triangle of rows n"""
     if n <= 0:
         return []
-    
+
     result = [[1]]
 
     for index in range(n - 1):
         """ add a zero to the  start & end of each row"""
-        temp = [0] +result[-1] + [0]
+        temp = [0] + result[-1] + [0]
         row = []
         for j in range(len(result[-1]) + 1):
-            row.append(temp[j] +temp[j + 1])
+            row.append(temp[j] + temp[j + 1])
         result.append(row)
     return result
--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-""" add all args to a python list and save them to file"""
-
-from sys import argv
+""" add all args to a python list and save them to file
+"""
+import sys
 """import functions"""
 save_to_json_file  = __import__("5-save_to_json").save_to_json
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
@@ -10,7 +10,7 @@ filename = "add_item.json"
 
 try:
     json_list = load_from_json_file(filename)
-except ValueError:
+except FileNotFoundError:
     json_list = []
 
 for arg in argv[1:]:
