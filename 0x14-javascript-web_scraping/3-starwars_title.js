ac80c225c72b07566c8a38f170f60ef6535a1be4
Lilwm <lmiiri95@gmail.com>
2022-11-15 15:38:02 +0000

    initial commit

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
-// JS Script
-require('request').get('http://swapi.co/api/films/' + process.argv[2] + '/', function (err, r, body) {
+const request = require('request');
+const num = process.argv[2];
+const url = (`https://swapi.co/api/films/${num}`);
+request(url, function (err, response, data) {
   if (err) {
     console.log(err);
   } else {
-    console.log(JSON.parse(body).title);
+    console.log(JSON.parse(data).title);
   }
-}); 
+});
